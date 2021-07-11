0d05ed802d28185c05d7cdc431eb73bb2a7dbf0c
Tanish Sarmah <70163670+tanishshadow@users.noreply.github.com>
2021-07-11 12:35:53 +0530

    Update 1.Number guesser.py

--- 1.Number guesser.py
+++ 1.Number guesser.py
@@ -10,11 +10,12 @@ start, stop = input("Enter the range:\n").split()
 start, stop = int(start), int(stop)
 
 n = random.randint(start, stop)  # generating the number
-form = math.log(stop-start+2, 2)  # the formula to create number of guesses
+form = len(range(start,stop+1)) // 5+1 # the formula to create number of guesses
 
 
 def main(n, form):
     form = int(form)
+    count = 1 
     print(
         f"Welcome to the number guessing game !!!\n\nYou have only {form} chances to guess!\n")
     while True:
@@ -22,14 +23,16 @@ def main(n, form):
         user = int(input("Guess the number:\n"))
 
         if n == user:
-            print(f"You won in {form} attempts !!")
+            print(f"You won in {count} attempts !!")
             break
         elif n < user:
             print("please try again the number you entered is greater")
             form -= 1
+            count += 1
         else:
             print("please try again the number you entered is less")
             form -= 1
+            count += 1
 
         if form == 0:
             print("game over!!!")
